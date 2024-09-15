a397e10ba70091e92309a73c06c7764e7bcb9245
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 20:37:37 +0100

    feat: Added functionality to create the tasks table.

--- src/database.ts
+++ src/database.ts
@@ -25,6 +25,20 @@ const createDatabase = async () => {
     )`);
 
 
+    await db.exec(`CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT NOT NULL,
+        completed INTEGER NOT NULL DEFAULT 0,
+        due_date TEXT,
+        priority INTEGER NOT NULL DEFAULT 3 CHECK(priority >= 1 AND priority <= 5),
+        user_id INTEGER,
+        category_id INTEGER,
+        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        FOREIGN KEY (category_id) REFERENCES categories(id)
+    )`);
+
+
     return db;
 };
 
