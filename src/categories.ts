b517224316b30dce30fcc70b6fffb426be8718be
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 20:44:58 +0100

    feat: Added functionality to add a category.

--- src/categories.ts
+++ src/categories.ts
@@ -1,8 +1,16 @@
 import createDatabase from './database';
 
 
+
 export type Category = {
     id: number;
     name: string;
     description?: string;
+};
+
+
+export const addCategory = async (name: string, description?: string): Promise<void> => {
+    const db = await createDatabase();
+    await db.run('INSERT INTO categories (name, description) VALUES (?, ?)', name, description);
+    console.log('Category added successfully!');
 };
